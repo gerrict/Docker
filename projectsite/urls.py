ff539ae004ccb93056072002d2a645723c24e188
gerrict <202040036@psu.palawan.edu.ph>
2026-06-05 10:59:55 +0800

    feat: complete docker containerization submission setup

---
+++ projectsite/urls.py
@@ -0,0 +1,60 @@
+"""
+URL configuration for projectsite project.
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/6.0/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.contrib import admin
+from django.urls import path, include
+from studentorg.views import (HomePageView, OrganizationList, OrganizationCreateView, OrganizationUpdateView, OrganizationDeleteView, 
+                            OrgMemberList, OrgMemberCreateView, OrgMemberUpdateView, OrgMemberDeleteView, 
+                            StudentListView, StudentCreateView, StudentUpdateView, StudentDeleteView,
+                            CollegeListView, CollegeCreateView, CollegeUpdateView, CollegeDeleteView,
+                            ProgramListView, ProgramCreateView, ProgramUpdateView, ProgramDeleteView,)
+from studentorg import views
+
+urlpatterns = [
+    path("admin/", admin.site.urls),
+    path("accounts/", include("allauth.urls")), # allauth routes
+    path('', views.HomePageView.as_view(), name='home'),
+    path('', include('pwa.urls')),
+    
+    #Organization
+    path('organization_list', OrganizationList.as_view(), name='organization-list'),
+    path('organization_list/add', OrganizationCreateView.as_view(), name='organization-add'),
+    path('organization_list/<pk>', OrganizationUpdateView.as_view(), name='organization-update'),
+    path('organization_list/<pk>/delete', OrganizationDeleteView.as_view(), name='organization-delete'),
+    
+    #OrgMember
+    path('orgmember_list', OrgMemberList.as_view(), name='orgmember-list'),
+    path('orgmember_list/add', OrgMemberCreateView.as_view(), name='orgmember-add'),
+    path('orgmember_list/<pk>', OrgMemberUpdateView.as_view(), name='orgmember-update'),
+    path('orgmember_list/<pk>/delete', OrgMemberDeleteView.as_view(), name='orgmember-delete'),
+    
+    #Student
+    path('student/', StudentListView.as_view(), name='student-list'),
+    path('student/add/', StudentCreateView.as_view(), name='student-add'),
+    path('student/<pk>/', StudentUpdateView.as_view(), name='student-update'),
+    path('student/<pk>/delete/', StudentDeleteView.as_view(), name='student-delete'),
+    
+    #College
+    path('college/', CollegeListView.as_view(), name='college-list'),
+    path('college/add/', CollegeCreateView.as_view(), name='college-add'),
+    path('college/<pk>/', CollegeUpdateView.as_view(), name='college-update'),
+    path('college/<pk>/delete/', CollegeDeleteView.as_view(), name='college-delete'),
+
+    #Program
+    path('program/', ProgramListView.as_view(), name='program-list'),
+    path('program/add/', ProgramCreateView.as_view(), name='program-add'),
+    path('program/<pk>/', ProgramUpdateView.as_view(), name='program-update'),
+    path('program/<pk>/delete/', ProgramDeleteView.as_view(), name='program-delete'),]
---
+++ readme.md
@@ -0,0 +1,7 @@
+# PSUSphere Containerized Setup
+
+## How to Run for Clients/Instructors
+1. Navigate to the `for_client` directory.
+2. Execute the following command in your terminal:
+   ```bash
+   docker-compose up -d
